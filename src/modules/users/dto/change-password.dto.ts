254c330227fd75015d8a3509f81b5ec1127a1f49
Tu <tuphan694@gmail.com>
2024-05-20 20:34:51 +0700

    feat: add example change password api

--- src/modules/users/dto/change-password.dto.ts
+++ src/modules/users/dto/change-password.dto.ts
@@ -1,12 +1,18 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class ChangePasswordDto {
-    @IsString()
-    @IsNotEmpty()
+    @ApiProperty({
+        example: 'Password123@',
+    })
+    @IsNotEmpty({ message: 'Current Password is required' })
+    @MinLength(8, { message: 'Current Password must be at least 8 characters' })
     currentPassword: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(8)
+    @ApiProperty({
+        example: 'Password123@',
+    })
+    @IsNotEmpty({ message: 'New Password is required' })
+    @MinLength(8, { message: 'New Password must be at least 8 characters' })
     newPassword: string;
 }
