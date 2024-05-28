82bc7099427a70ec41ee40cf978d540ba36ec001
Tu <tuphan694@gmail.com>
2024-05-28 21:54:12 +0700

    fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/doc', app, document);
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
